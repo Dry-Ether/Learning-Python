810af265e579c57d622ea44aebf71e7adc004b6a
Dry_Ether <mail2adibmahmood@gmail.com>
2025-06-18 00:44:44 +0530

    Data Types int float boolen

--- DataTypes.py
+++ DataTypes.py
@@ -1,2 +1,23 @@
 var_1 = 3
-print(type(var_1))
+print("Ver 1=" + str(type(var_1)))
+var_2= 12.3
+print("Ver 2=" + str(type(var_2)))
+print(var_1 + var_2)
+
+
+var_1 = 0o123
+print("Ver 1=" + str(type(var_1)))
+var_2= 0x123
+print("Ver 2=" + str(type(var_2)))
+print(var_1 + var_2)
+
+
+name="Adib"
+print(name[0])   #for use as a string numbering 
+
+print(5 *"Adib\'s \"book\" \\n") #using \ for skipping command and we can multiply this by* 
+
+
+var=True
+print(var)
+print(type(var))
