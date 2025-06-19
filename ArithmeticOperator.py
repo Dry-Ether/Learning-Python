205123a38e551cf39b50da83aaf7bc4de7928409
Dry_Ether <mail2adibmahmood@gmail.com>
2025-06-19 20:27:46 +0530

    adjust

--- ArithmeticOperator.py
+++ ArithmeticOperator.py
@@ -11,7 +11,7 @@ print(5+2*3-4/2)  # Combined operations with different preced
 print(5+2*3-4/2**2)  # Exponentiation has the highest precedence
 print(5+2*3-4/2**2%3)  # Modulus has lower precedence than exponentiation but higher than addition and subtraction
 print(5+2*3-4/2**2%3//1)  # Floor division has lower precedence than modulus but higher than addition and subtraction
-print(5+2*3-4/2**2%3//1*2   )  # Multiplication has the same precedence as addition and subtraction, evaluated left to right
+print(5+2*3-4/2**2%3//1*2)  # Multiplication has the same precedence as addition and subtraction, evaluated left to right
 print(5+2*3-4/2**2%3//1*2+1)  # Addition has the same precedence as multiplication, evaluated left to right
 print(5+2*3-4/2**2%3//1*2+1-3)  # Subtraction has the same precedence as addition, evaluated left to right
 print(5+2*3-4/2**2%3//1*2+1-3*2)  # Multiplication has the same precedence as addition and subtraction, evaluated left to right
