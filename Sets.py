c5c0a6f049e599a9e5eb2cbbf2ceb01b01984298
Dry_Ether <mail2adibmahmood@gmail.com>
2025-08-18 04:56:35 +0530

    New Start

---
+++ Sets.py
@@ -0,0 +1,13 @@
+#set1={10,5,85,True,"Adib",10,1}
+#print(set1)
+#print(set1.pop())
+#print(set1)
+#set1.difference_update(['mahmood','Adib'])
+#print(set1)
+
+set3={1,2,3}
+set4={1,2,3,4,5,6}
+print(set3.issubset(set4))
+print(set3<=set4)
+print(set4.issuperset(set3))
+print(set4>=set3)
